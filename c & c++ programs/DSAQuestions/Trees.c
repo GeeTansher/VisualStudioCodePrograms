55ecd5c253d57336dd27b3303ae8dd04b6c15222
Geetansh Verma <vrgpverma@gmail.com>
2021-12-18 02:23:22 +0530

    first

--- c & c++ programs/DSAQuestions/Trees.c
+++ c & c++ programs/DSAQuestions/Trees.c
@@ -1,46 +1,46 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Tree
 {
     struct Tree *left;
     int data;
     struct Tree *right;
-}tree;
+} tree;
 
 void preorder(tree *root)
 {
-    if(root!=NULL)
+    if (root != NULL)
     {
-        printf("%d  ",root->data);
+        printf("%d  ", root->data);
         preorder(root->left);
         preorder(root->right);
     }
-    else 
+    else
         return;
 }
 
 void inorder(tree *root)
 {
-    if(root!=NULL)
+    if (root != NULL)
     {
         preorder(root->left);
-        printf("%d  ",root->data);
+        printf("%d  ", root->data);
         preorder(root->right);
     }
-    else 
+    else
         return;
 }
 
 void postorder(tree *root)
 {
-    if(root!=NULL)
+    if (root != NULL)
     {
         preorder(root->left);
         preorder(root->right);
-        printf("%d  ",root->data);
+        printf("%d  ", root->data);
     }
-    else 
+    else
         return;
 }
 
@@ -71,29 +71,42 @@ void postorder(tree *root)
 
 int countnodes(tree *root)
 {
-    
+    int ctr = 0;
+    if (root == NULL)
+        return 0;
+    ctr = countnodes(root->left) + countnodes(root->right);
+    return ctr + 1;
 }
 
-void insert(tree **root,int num)
+int countLeafNodes(tree *root)
+{
+    int ctr = 1;
+    if (root->left == NULL && root->right == NULL)
+        return ctr;
+    else
+        return countLeafNodes(root->left) + countLeafNodes(root->right);
+}
+
+void insert(tree **root, int num)
 {
     tree *p;
-    p=(tree*)malloc(sizeof(struct Tree));
-    p->data=num;
-    if(*root==NULL)
+    p = (tree *)malloc(sizeof(struct Tree));
+    p->data = num;
+    if (*root == NULL)
     {
-        p->left=NULL;
-        p->right=NULL;
-        *root=p;
+        p->left = NULL;
+        p->right = NULL;
+        *root = p;
     }
     else
     {
-        if(p->data>(*root)->data)
+        if (p->data > (*root)->data)
         {
-            insert(&((*root)->right),num);
+            insert(&((*root)->right), num);
         }
         else
         {
-            insert(&((*root)->left),num);
+            insert(&((*root)->left), num);
         }
     }
 }
@@ -101,56 +114,63 @@ void insert(tree **root,int num)
 int main()
 {
     int num;
-    tree *root=NULL,*temp=NULL;
-    int choice,choice1;
+    tree *root = NULL, *temp = NULL;
+    int ctr;
+    int choice, choice1;
     do
     {
-        printf("\nEnter the choice:\n1. Insert\n2. Delete\n3. Display\n4. Exit\n");
-        scanf("%d",&choice);
-        switch(choice)
+        printf("\nEnter the choice:\n1. Insert\n2. Count nodes\n3. Display\n4. Count leaf nodes\n5. Exit\n");
+        scanf("%d", &choice);
+        switch (choice)
         {
+        case 1:
+            printf("\nEnter the value to be inserted:");
+            scanf("%d", &num);
+            // if(root=NULL)
+            // {
+            //     temp=insert(root,num);
+            //     root=temp;
+            // }
+            // else
+            // {
+            //     temp=insert(root,num);
+            // }
+            insert(&root, num);
+            break;
+        case 2:
+            ctr = countnodes(root);
+            printf("No of nodes:%d", ctr);
+            break;
+        case 3:
+            printf("\nIn which order you want to print:\n1. PreOrder\n2. InOreder\n3. PostOrder\n");
+            scanf("%d", &choice1);
+            switch (choice1)
+            {
             case 1:
-                printf("\nEnter the value to be inserted:");
-                scanf("%d",&num);
-                // if(root=NULL)
-                // {
-                //     temp=insert(root,num);
-                //     root=temp;
-                // }
-                // else
-                // {
-                //     temp=insert(root,num);
-                // }
-                insert(&root,num);
+                preorder(root);
                 break;
             case 2:
+                inorder(root);
                 break;
             case 3:
-                printf("\nIn which order you want to print:\n1. PreOrder\n2. InOreder\n3. PostOrder\n");
-                scanf("%d",&choice1);
-                switch(choice1)
-                {
-                    case 1:
-                        preorder(root);
-                        break;
-                    case 2:
-                        inorder(root);
-                        break;
-                    case 3:
-                        postorder(root);
-                        break;
-                    default:
-                        printf("Wrong input");
-                        break;
-                }
+                postorder(root);
                 break;
-            case 4:
-                exit(0);
             default:
-                printf("Wrong input...");
+                printf("Wrong input");
                 break;
+            }
+            break;
+        case 4:
+            ctr = countLeafNodes(root);
+            printf("No of nodes:%d", ctr);
+            break;
+        case 5:
+            exit(0);
+        default:
+            printf("Wrong input...");
+            break;
         }
 
-    }while(1);
+    } while (1);
     return 0;
 }
