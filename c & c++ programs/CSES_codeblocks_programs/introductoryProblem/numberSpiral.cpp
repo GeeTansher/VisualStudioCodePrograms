feb98c0bb72c4c914f6052c270e960d75a6071f0
Geetansh Verma <vrgpverma@gmail.com>
2022-01-27 01:55:10 +0530

    new

--- c & c++ programs/CSES_codeblocks_programs/introductoryProblem/numberSpiral.cpp
+++ c & c++ programs/CSES_codeblocks_programs/introductoryProblem/numberSpiral.cpp
@@ -3,11 +3,37 @@ using namespace std;
 
 int main()
 {
-    long int t,y,x;
+    int t;
+    long long int y,x;
+    long long int m,num;
     cin>>t;
     while(t--)
     {
         cin>>y>>x;
+        m=max(y,x);
+        if(m==y)
+        {
+            if(m%2==0)
+            {
+                num=(m*m)-x+1;
+            }
+            else
+            {
+                num=((m-1)*(m-1)+1)+x-1;
+            }
+        }
+        else
+        {
+            if(m%2!=0)
+            {
+                num=(m*m)-y+1;
+            }
+            else
+            {
+                num=((m-1)*(m-1)+1)+y-1;
+            }
+        }
+        cout<<num<<endl;
     }
     return 0;
 }
