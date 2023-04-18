756894a3bd8399e8245493ffdc2fae9c29437186
Geetansh verma <verma.geetansh02@gmail.com>
2023-04-18 17:24:58 +0530

    lex more

--- lex programs/lex.yy.c
+++ lex programs/lex.yy.c
@@ -282,11 +282,12 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
-static yyconst short int yy_accept[9] =
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
+static yyconst short int yy_accept[16] =
     {   0,
-        0,    0,    4,    2,    3,    1,    1,    0
+        0,    0,    5,    3,    4,    3,    3,    3,    0,    0,
+        0,    0,    2,    1,    0
     } ;
 
 static yyconst int yy_ec[256] =
@@ -295,8 +296,8 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
+        1,    1,    3,    1,    4,    5,    1,    6,    6,    6,
+        6,    6,    6,    6,    6,    6,    6,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -321,29 +322,33 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[4] =
+static yyconst int yy_meta[7] =
     {   0,
-        1,    1,    2
+        1,    1,    1,    1,    1,    2
     } ;
 
-static yyconst short int yy_base[10] =
+static yyconst short int yy_base[20] =
     {   0,
-        0,    0,    5,    6,    6,    0,    0,    6,    2
+        0,    0,   13,   14,   14,    0,    0,    7,    0,    6,
+        0,    0,    0,    0,   14,    8,    7,    6,    5
     } ;
 
-static yyconst short int yy_def[10] =
+static yyconst short int yy_def[20] =
     {   0,
-        8,    1,    8,    8,    8,    9,    9,    0,    8
+       15,    1,   15,   15,   15,   16,   17,   16,    8,   17,
+       18,   19,   18,   19,    0,   15,   15,   15,   15
     } ;
 
-static yyconst short int yy_nxt[10] =
+static yyconst short int yy_nxt[21] =
     {   0,
-        4,    5,    6,    7,    8,    3,    8,    8,    8
+        4,    5,    6,    7,    4,    8,   14,   13,   10,    9,
+       12,   11,   15,    3,   15,   15,   15,   15,   15,   15
     } ;
 
-static yyconst short int yy_chk[10] =
+static yyconst short int yy_chk[21] =
     {   0,
-        1,    1,    1,    9,    3,    8,    8,    8,    8
+        1,    1,    1,    1,    1,    1,   19,   18,   17,   16,
+       10,    8,    3,   15,   15,   15,   15,   15,   15,   15
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -357,12 +362,12 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 ".\\perfectSquare.l"
+#line 1 ".\\roundOffFloat.l"
 #define INITIAL 0
-#line 2 ".\\perfectSquare.l"
+#line 2 ".\\roundOffFloat.l"
     #include<stdio.h>
-    int i,flag=0,num;
-#line 366 "lex.yy.c"
+    int ans; float num;
+#line 371 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -513,9 +518,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 6 ".\\perfectSquare.l"
+#line 6 ".\\roundOffFloat.l"
 
-#line 519 "lex.yy.c"
+#line 524 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -566,13 +571,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 9 )
+				if ( yy_current_state >= 16 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 14 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -600,34 +605,33 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 7 ".\\perfectSquare.l"
+#line 7 ".\\roundOffFloat.l"
 {
-            num=atoi(yytext);
-            for(i=0;i*i<=num;i++){
-                if(i*i==num){
-                    flag=1;
-                    break;
-                }
-            }
-            if(flag==1){
-                printf("Perfect square");
-            }
-            else{
-                printf("Not a perfect square");
-            }
-        }
+                            num=atof(yytext);
+                            ans=(int)(num-0.5);
+                            printf("The nearest int is: %d", ans);
+                        }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 22 ".\\perfectSquare.l"
-;
+#line 14 ".\\roundOffFloat.l"
+{
+                            num=atof(yytext);
+                            ans=(int)(num+0.5);
+                            printf("The nearest int is: %d", ans);
+                        }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 23 ".\\perfectSquare.l"
+#line 20 ".\\roundOffFloat.l"
+{printf("Invalid");}
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 21 ".\\roundOffFloat.l"
 ECHO;
 	YY_BREAK
-#line 631 "lex.yy.c"
+#line 635 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -919,7 +923,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 9 )
+			if ( yy_current_state >= 16 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -954,11 +958,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 9 )
+		if ( yy_current_state >= 16 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 8);
+	yy_is_jam = (yy_current_state == 15);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1513,7 +1517,7 @@ int main()
 	return 0;
 	}
 #endif
-#line 23 ".\\perfectSquare.l"
+#line 21 ".\\roundOffFloat.l"
 
 
 int main(){
